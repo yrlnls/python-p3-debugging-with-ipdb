eb86b56cb7cf1d2b5586d95be570e942f4af9018
tillenotuoma <totuoma27@gmail.com>
2025-02-11 01:13:57 +0300

    passed tests

--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -3,12 +3,16 @@
 import ipdb
 
 def tracing_the_function():
+    """
+    This function demonstrates the use of ipdb for debugging.
+    It sets a breakpoint and prints a variable after the breakpoint.
+    """
     inside_the_function = "We're inside the function"
     print(inside_the_function)
+    this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
     print("We're about to stop because of ipdb!")
     ipdb.set_trace()
-    this_variable_hasnt_been_interpreted_yet = \
-        "The program froze before it could read me!"
     print(this_variable_hasnt_been_interpreted_yet)
+
 
 tracing_the_function()
